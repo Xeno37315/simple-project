e9b21fe92a54dc264e72235903d16f6152daaa2e
Xeno37315 <110820878+Xeno37315@users.noreply.github.com>
2022-08-08 16:50:51 +0300

    sim_proj_0.13

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('everything is working.')
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+
+import * as router from "./benjamin.js";
+
+const port = 5000
+app.listen(port, function() {
+    console.log(`Server is running on port: ${port}.`)
+})
